58111d9a8b925357f73454d4a0b0a71bab710939
Alex P <stealth.ip@gmail.com>
2016-08-29 22:47:15 +0300

    spring boot initialize

---
+++ carsservice/src/main/java/com/cservice/CarsserviceApplication.java
@@ -0,0 +1,12 @@
+package com.cservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsserviceApplication.class, args);
+	}
+}
