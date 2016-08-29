f0ad5373afac6f31f7eece4d9a80c1c513fcda87
Alex P <stealth.ip@gmail.com>
2016-08-29 23:00:19 +0300

    added ping method

--- carsservice/src/main/java/com/cservice/CarsserviceApplication.java
+++ carsservice/src/main/java/com/cservice/CarsserviceApplication.java
@@ -1,12 +1,33 @@
 package com.cservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@CrossOrigin
+@RestController
+@ComponentScan
+@EnableAutoConfiguration
+@RequestMapping(value = "api", produces = MediaType.APPLICATION_JSON_VALUE)
 public class CarsserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CarsserviceApplication.class, args);
 	}
+
+
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "/ping",method = RequestMethod.GET)
+	public void ping(){
+
+	}
+
+
+
+
 }
