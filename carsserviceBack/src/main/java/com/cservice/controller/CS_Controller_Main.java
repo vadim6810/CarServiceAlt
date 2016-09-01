26f85ab9c5177813577fe91353d25487259ecc57
Alex P <stealth.ip@gmail.com>
2016-09-01 23:47:24 +0300

    add Contractor Entity

--- carsserviceBack/src/main/java/com/cservice/controller/CS_Controller_Main.java
+++ carsserviceBack/src/main/java/com/cservice/controller/CS_Controller_Main.java
@@ -1,5 +1,6 @@
 package com.cservice.controller;
 
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 
 @SpringBootApplication
 @CrossOrigin
@@ -28,6 +32,21 @@ public class CS_Controller_Main {
 	}
 
 
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "login",method = RequestMethod.POST)//us test
+	public void login(HttpServletRequest request, HttpServletResponse response){
 
+
+		response.addCookie(new Cookie("id","121212"));
+
+	}
+
+	@ResponseStatus(HttpStatus.OK)
+	@RequestMapping(value = "logout",method = RequestMethod.POST)//us test
+	public void logout(HttpServletRequest request, HttpServletResponse response){
+
+
+
+	}
 
 }
